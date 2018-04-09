d20de25e29875492fbd6736666e8807fa2b9a318
Andrei Bogomja <bogomja@gmail.com>
2018-04-09 23:06:10 +0100

    Add terraform resource config

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,20 @@ provider "google" {
   project = "inductive-actor-198011"
   region  = "europe-west2-a"
 }
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "reddit-base"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+}
