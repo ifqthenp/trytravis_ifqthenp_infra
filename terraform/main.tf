12b5610e7e11fd1e358ab9b8c7caefa52373c770
Andrei Bogomja <bogomja@gmail.com>
2018-04-10 10:18:40 +0100

    Define private_key_path variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "google_compute_instance" "app" {
     type        = "ssh"
     user        = "appuser"
     agent       = false
-    private_key = "${file("~/.ssh/appuser")}"
+    private_key = "${file(var.private_key_path)}"
   }
 
   provisioner "file" {
