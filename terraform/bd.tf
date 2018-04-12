edd69dba1440651ea0bfbcd21ecf9b531da2444d
Andrei Bogomja <bogomja@gmail.com>
2018-04-12 19:10:11 +0100

    Add db.tf config file

---
+++ terraform/bd.tf
@@ -0,0 +1,40 @@
+resource "google_compute_instance" "db" {
+  name         = "reddit-db"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-db"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.db_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
