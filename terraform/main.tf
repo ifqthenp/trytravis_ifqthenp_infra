99d04e53396b20ee34fc13823e3b988f9f466878
Andrei Bogomja <bogomja@gmail.com>
2018-04-12 20:06:21 +0100

    Add modules to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,24 @@ provider "google" {
   project = "${var.project}"
   region  = "${var.region}"
 }
+
+module "app" {
+  source          = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+}
+
+module "db" {
+  source          = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.app_disk_image}"
+}
+
+module "vpc" {
+  source          = "modules/vpc"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.app_disk_image}"
+}
