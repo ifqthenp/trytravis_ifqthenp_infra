2b61b3d11ac6305c218a9756e96d1eec9c44f813
Andrei Bogomja <bogomja@gmail.com>
2018-04-12 21:51:06 +0100

    Add app, db, vpc modules

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,13 @@
+variable zone {
+  description = "The zone that the machine should be created in"
+  default     = "europe-west2-a"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,13 @@
+variable zone {
+  description = "The zone that the machine should be created in"
+  default     = "europe-west2-a"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
