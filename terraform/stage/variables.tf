f73d477bc9dc26812212105ac10115fee7806746
Andrei Bogomja <bogomja@gmail.com>
2018-04-13 14:46:26 +0100

    stage configuration is ready to use

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -16,20 +16,26 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-# variable private_key_path {
-#   description = "The contents of an SSH key to use for the connection"
-# }
-
-# variable disk_image {
-#   description = "Disk image"
-# }
+variable private_key_path {
+  description = "The contents of an SSH key to use for the connection"
+}
 
 variable app_disk_image {
   description = "Disk image for reddit app"
   default     = "reddit-app-base"
 }
 
+variable app_machine_type {
+  description = "Machine type for app instance"
+  default     = "f1-micro"
+}
+
 variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
+}
+
+variable db_machine_type {
+  description = "Machine type for db instance"
+  default     = "f1-micro"
 }
