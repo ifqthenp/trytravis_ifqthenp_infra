c1910f88bc1190df4db286ce452db4863dd05b4c
Andrei Bogomja <bogomja@gmail.com>
2018-04-13 09:37:15 +0100

    Move resources for firewall rules from app and db to vpc module

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -21,16 +21,3 @@ resource "google_compute_instance" "db" {
   }
 }
 
-resource "google_compute_firewall" "firewall_mongo" {
-  name = "allow-mongo-default"
-
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["27017"]
-  }
-
-  target_tags = ["reddit-db"]
-  source_tags = ["reddit-app"]
-}
