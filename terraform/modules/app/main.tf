f956cfbd91464d976a661f4bfb5a6da0667246ae
Andrei Bogomja <bogomja@gmail.com>
2018-04-13 09:35:26 +0100

    Add config for app provisioning

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -21,6 +21,22 @@ resource "google_compute_instance" "app" {
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.puma.rendered}"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/files/deploy.sh"
+  }
 }
 
 resource "google_compute_address" "app_ip" {
