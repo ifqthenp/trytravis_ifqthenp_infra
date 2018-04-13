686629d93c66cd3582cbd5b7b72e28be046a38c7
Andrei Bogomja <bogomja@gmail.com>
2018-04-13 16:22:17 +0100

    Change storage bucket name

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -5,10 +5,11 @@ provider "google" {
 }
 
 module "storage-bucket" {
-  source  = "SweetOps/storage-bucket/google"
-  version = "0.1.1"
+  force_destroy = true
+  source        = "SweetOps/storage-bucket/google"
+  version       = "0.1.1"
 
-  name = ["my_test_10001", "my_test_10002"]
+  name = ["ifqthenp_tfstate_bucket", "my_test_10002"]
 }
 
 output storage-bucket_url {
